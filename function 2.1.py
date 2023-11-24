1fa8bd8fca0518bc9a36d0a1ec4fddc3f51e724d
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-24 21:34:32 +0545

    explicit_typeconversion_in_python

---
+++ function 2.1.py
@@ -0,0 +1,35 @@
+#explicit type conversion
+
+x=5
+y=2
+a=x/y
+d=int(a)   # converts float to int 
+print(d)
+
+a=5
+b='10'
+c=a+int(b)  #possible in explicit coonversion 
+print(c)
+
+
+#float to integer 
+
+m=10.56
+n=int(m)
+print(n)
+
+
+#integer to float
+
+j=10
+k=float(j)
+l=complex(j)   #integer to complex
+t=str(j)       #integer to string
+u=bin(j)       #integer to binaary
+print(k)
+print(l)
+print(t)
+print(u)
+
+
+
